154f1b5940fc41a56e75c79100467982a0f8a6d1
Mike Ortiz <emike108@aim.com>
2021-10-07 12:16:41 -0700

    delete function implemented

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,21 +3,60 @@ import ReacDOM from 'react-dom';
 import GroceryList from './GroceryList.jsx';
 import groceriesData from '../data/groceriesData.js';
 
-const App = () => (
-  <div>
-    <img src="grocery-bags.png" />
-    <h1>Grocery List</h1>
-    <form>
-      <label> Item
-        <input name="item" value="" />
-      </label>
-      <label> Quantity
-        <input name="quantity" value="" />
-      </label>
-      <button>Add Grocery</button>
-      <GroceryList groceries={groceriesData} />
-    </form>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groceriesData: groceriesData
+    }
+
+    this.deleteClick = this.deleteClick.bind(this);
+  }
+
+  deleteClick(name) {
+    // event.preventDefault();
+    //look at what item the click even occured
+    //delete that item from the data
+    this.setState({groceriesData: this.state.groceriesData.filter(item => item.name !== name)});
+    // console.log('delete click', event, name);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <img src="grocery-bags.png" />
+        <h1>Grocery List</h1>
+        <form>
+          <label> Item
+            <input name="item" value="" />
+          </label>
+          <label> Quantity
+            <input name="quantity" value="" />
+          </label>
+          <button>Add Grocery</button>
+        </form>
+          <GroceryList groceries={this.state.groceriesData} deleteClick={this.deleteClick} />
+      </div>
+    )
+  }
+};
 
 export default App;
+
+// const App = () => (
+//   <div>
+//     <img src="grocery-bags.png" />
+//     <h1>Grocery List</h1>
+//     <form>
+//       <label> Item
+//         <input name="item" value="" />
+//       </label>
+//       <label> Quantity
+//         <input name="quantity" value="" />
+//       </label>
+//       <button>Add Grocery</button>
+//       <GroceryList groceries={groceriesData} />
+//     </form>
+//   </div>
+// );
--- client/src/components/GroceryListItem.jsx
+++ client/src/components/GroceryListItem.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReacDOM from 'react-dom';
 
 
-const GroceryListItem = (prop) => (
+const GroceryListItem = (props) => (
   <li>
-    <span>{prop.item.name}</span>
-    <span>{prop.item.quantity}</span>
+    <span>{props.item.name}</span>
+    <span> - {props.item.quantity}</span>
+    <button onClick={() => props.deleteClick(props.item.name)} >Delete</button>
   </li>
 );
 
